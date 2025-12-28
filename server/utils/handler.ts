cfb66a964771b705ce0bf82b06bbf082a472d846
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-12-28 23:21:24 +0530

    feat(core): add CustomError, email utility, logger, and async handler

--- server/utils/handler.ts
+++ server/utils/handler.ts
@@ -1,15 +1,15 @@
 import type { NextFunction, Request, Response } from "express";
 
 export type AsyncHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction,
 ) => Promise<void>;
 
 const handler =
-  (fn: AsyncHandler) =>
-  (req: Request, res: Response, next: NextFunction): void => {
-    fn(req, res, next).catch(next);
-  };
+	(fn: AsyncHandler) =>
+	(req: Request, res: Response, next: NextFunction): void => {
+		fn(req, res, next).catch(next);
+	};
 
 export default handler;
