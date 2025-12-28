6914bc2a360a5ba5df9ad83bb0f0a3e48a3df1dc
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-12-28 23:09:49 +0530

    chore(utils): add async handler wrapper for Express

---
+++ server/utils/handler.ts
@@ -0,0 +1,15 @@
+import type { NextFunction, Request, Response } from "express";
+
+export type AsyncHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void>;
+
+const handler =
+  (fn: AsyncHandler) =>
+  (req: Request, res: Response, next: NextFunction): void => {
+    fn(req, res, next).catch(next);
+  };
+
+export default handler;
