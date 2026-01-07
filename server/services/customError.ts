1d1ea070735bd8ccad12e0e9fa57a81af168a79d
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-08 02:05:45 +0530

    refactor(core): remove prisma, custom error and utils base files

--- server/services/customError.ts
+++
@@ -1,9 +0,0 @@
-export class CustomError extends Error {
-	statusCode: number;
-
-	constructor(message: string, statusCode = 500) {
-		super(message);
-		this.statusCode = statusCode;
-		Object.setPrototypeOf(this, CustomError.prototype);
-	}
-}
