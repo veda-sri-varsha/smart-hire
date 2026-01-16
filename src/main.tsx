b0128c8d04771d148de428200c60799357de63fc
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-16 23:11:36 +0530

    refactor(main): add global header and safe root initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Header from "./components/header";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-	createRoot(rootElement).render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-	);
+const container = document.getElementById("root");
+
+if (!container) {
+	throw new Error("Root container missing in index.html");
 }
+
+const root = ReactDOM.createRoot(container);
+
+root.render(
+	<BrowserRouter>
+		<Header />
+		<Routes>
+			<Route path="/" element={<App />} />
+		</Routes>
+	</BrowserRouter>,
+);
