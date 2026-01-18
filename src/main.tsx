719ff9c8f0e24fd0f413a48bdd0acddc691cdf3d
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-18 22:51:01 +0530

    fix: resolve Biome noNonNullAssertion lint error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import Header from "./components/header";
+import "./App.css"; 
+import Header from "./components/Header";
 
-const container = document.getElementById("root");
-
-if (!container) {
-	throw new Error("Root container missing in index.html");
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <Header/>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(container);
-
-root.render(
-	<BrowserRouter>
-		<Header />
-		<Routes>
-			<Route path="/" element={<App />} />
-		</Routes>
-	</BrowserRouter>,
-);
