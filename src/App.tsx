2232091101939116098f33f763100eb1e3282b11
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-20 23:23:41 +0530

    feat(components): add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 const App = () => {
-  return (
-    <div>
-      <h1>Smart-Hire Platform</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Smart-Hire Platform</h1>
+			<h2>Hero-section and More Components are coming soon</h2>
+		</div>
+	);
 };
 
 export default App;
