9401e49f6f60af86f9343daa51ea55cce612582a
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-22 22:20:13 +0530

    feat(components): add complete Footer and SCSS modules

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,58 @@
-const Footer = () => {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  );
-};
+import { Link } from "@tanstack/react-router";
+import styles from "@/components/Footer/Footer.module.css";
+import Button from "@/components/ui/Button";
 
-export default Footer;
+import { companyLinks, jobCategories } from "@/constants/FooterLinks";
+import { JobIcon } from "../Icons";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div>
+          <JobIcon />
+          <h3>Job</h3>
+          <p>Quis enim pellentesque viverra tellus eget malesuada facilisis.</p>
+        </div>
+
+        <div>
+          <h4>Company</h4>
+          <ul>
+            {companyLinks.map((link) => (
+              <li key={link.name}>
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h4>Job Categories</h4>
+          <ul>
+            {jobCategories.map((category) => (
+              <li key={category.name}>
+                <Link to={category.path}>{category.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>
+          <h4>Newsletter</h4>
+          <form>
+            <input placeholder="Email Address" aria-label="Email Address" />
+            <Button type="submit">Subscribe now</Button>
+          </form>
+        </div>
+      </div>
+
+      <div className={styles.bottom}>
+        <p>© Copyright Job Portal 2024</p>
+        <div>
+          <Link to="/">Privacy Policy</Link>
+          <Link to="/">Terms & Conditions</Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
