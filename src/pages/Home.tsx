0601e2a1b08b3a759a21511b4dd4de5cf02c203a
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-24 00:10:26 +0530

    feat(home): add Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero/Hero";
+import Testimonials from "@/components/Testimonials";
+
+export default function Home() {
+	return (
+		<>
+			<Hero />
+			<Testimonials />
+		</>
+	);
+}
