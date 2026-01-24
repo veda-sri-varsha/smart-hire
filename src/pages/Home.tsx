f917004309567aaa2f68e48838a0329a4a773999
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-24 21:56:14 +0530

    feat(home): add recent jobs section to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero/Hero";
+import RecentJobs from "@/components/RecentJobs";
 import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<RecentJobs />
 			<Testimonials />
 		</>
 	);
