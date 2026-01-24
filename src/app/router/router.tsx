d3782a2e4bdea9cea315f20404d5aa5c11899d88
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-24 21:55:24 +0530

    refactor(router): organize root and index routes

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,19 +1,19 @@
 import {
-  createRootRoute,
-  createRoute,
-  createRouter,
+	createRootRoute,
+	createRoute,
+	createRouter,
 } from "@tanstack/react-router";
 import Home from "@/pages/Home";
 import MainLayout from "../../layouts/MainLayout";
 
 const rootRoute = createRootRoute({
-  component: MainLayout,
+	component: MainLayout,
 });
 
 const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/",
-  component: Home,
+	getParentRoute: () => rootRoute,
+	path: "/",
+	component: Home,
 });
 
 const routeTree = rootRoute.addChildren([indexRoute]);
@@ -21,7 +21,7 @@ const routeTree = rootRoute.addChildren([indexRoute]);
 export const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
