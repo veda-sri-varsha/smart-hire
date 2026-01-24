0a25b86664069bb0a02d26dddc38fcbca328b98b
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-24 21:54:40 +0530

    feat(components): implement company logos showcase section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,49 +1,67 @@
-import { CompaniesIcon, CompanyIcon, UserIcon } from "../Icons";
+import {
+	AdobeIcon,
+	CompaniesIcon,
+	CompanyIcon,
+	LinearIcon,
+	SlackIcon,
+	Spotify,
+	UserIcon,
+} from "../Icons";
 import Button from "../ui/Button";
 import styles from "./Hero.module.scss";
 
 export default function Hero() {
 	return (
-		<section className={styles.hero}>
-			<div className={styles.overlay} />
+		<>
+			<section className={styles.hero}>
+				<div className={styles.overlay} />
 
-			<div className={styles.content}>
-				<h1>Find Your Dream Job Today!</h1>
-				<p>
-					Connecting Talent with Opportunity: Your Gateway to Career Success
-				</p>
+				<div className={styles.content}>
+					<h1>Find Your Dream Job Today!</h1>
+					<p>
+						Connecting Talent with Opportunity: Your Gateway to Career Success
+					</p>
 
-				<div className={styles.searchBox}>
-					<input placeholder="Job Title or Company" />
-					<select title="Location">
-						<option>Select Location</option>
-					</select>
-					<select title="Category">
-						<option>Select Category</option>
-					</select>
-					<Button>
-						<span>Search Job</span>
-					</Button>
+					<div className={styles.searchBox}>
+						<input placeholder="Job Title or Company" />
+						<select title="Location">
+							<option>Select Location</option>
+						</select>
+						<select title="Category">
+							<option>Select Category</option>
+						</select>
+						<Button>
+							<span>Search Job</span>
+						</Button>
+					</div>
+
+					<div className={styles.stats}>
+						<div>
+							<CompanyIcon />
+							<span>25,850</span>
+							<p>Jobs</p>
+						</div>
+						<div>
+							<UserIcon />
+							<span>10,250</span>
+							<p>Candidates</p>
+						</div>
+						<div>
+							<CompaniesIcon />
+							<span>18,400</span>
+							<p>Companies</p>
+						</div>
+					</div>
 				</div>
-
-				<div className={styles.stats}>
-					<div>
-						<CompanyIcon />
-						<span>25,850</span>
-						<p>Jobs</p>
-					</div>
-					<div>
-						<UserIcon />
-						<span>10,250</span>
-						<p>Candidates</p>
-					</div>
-					<div>
-						<CompaniesIcon />
-						<span>18,400</span>
-						<p>Companies</p>
-					</div>
+			</section>
+			<section className={styles.logoStrip}>
+				<div className={styles.logoContainer}>
+					<Spotify />
+					<SlackIcon />
+					<AdobeIcon />
+					<LinearIcon />
 				</div>
-			</div>
-		</section>
+			</section>
+		</>
 	);
 }
