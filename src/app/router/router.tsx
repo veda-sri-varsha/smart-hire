3164b883b8754c45a5ced3593b4d0ce88faec800
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-27 22:05:45 +0530

    chore(router): set up root and jobs routes with MainLayout

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,27 +1,34 @@
 import {
-	createRootRoute,
-	createRoute,
-	createRouter,
+  createRootRoute,
+  createRoute,
+  createRouter,
 } from "@tanstack/react-router";
 import Home from "@/pages/Home";
+import Jobs from "@/pages/Jobs/Jobs";
 import MainLayout from "../../layouts/MainLayout";
 
 const rootRoute = createRootRoute({
-	component: MainLayout,
+  component: MainLayout,
 });
 
 const indexRoute = createRoute({
-	getParentRoute: () => rootRoute,
-	path: "/",
-	component: Home,
+  getParentRoute: () => rootRoute,
+  path: "/",
+  component: Home,
 });
 
-const routeTree = rootRoute.addChildren([indexRoute]);
+const jobsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/jobs",
+  component: Jobs,
+});
+
+const routeTree = rootRoute.addChildren([indexRoute, jobsRoute]);
 
 export const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
-	interface Register {
-		router: typeof router;
-	}
+  interface Register {
+    router: typeof router;
+  }
 }
