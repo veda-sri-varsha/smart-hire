d0cbfeaff4a8576e6a14434f0fdc0b463d773534
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-27 22:07:39 +0530

    feat(header): integrate TanStack Router links in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,36 @@
 import { JobIcon } from "../Icons";
 import "./Header.scss";
+import { Link } from "@tanstack/react-router";
 
 const Header = () => {
-	return (
-		<header className="header">
-			<div className="header__container">
-				<div className="header__logo">
-					<span className="header__logo-icon">
-						<JobIcon />
-					</span>
-					<span className="header__logo-text">Job Portal</span>
-				</div>
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo">
+          <span className="header__logo-icon">
+            <JobIcon />
+          </span>
+          <span className="header__logo-text">Job Portal</span>
+        </div>
 
-				<nav className="header__nav">
-					<a href="/">Home</a>
-					<a href="/">Jobs</a>
-					<a href="/">About Us</a>
-					<a href="/">Contact Us</a>
-				</nav>
+        <nav className="header__nav">
+          <Link to="/">Home</Link>
+          <Link to="/jobs">Jobs</Link>
+          <Link to="/">About Us</Link>
+          <Link to="/">Contact Us</Link>
+        </nav>
 
-				<div className="header__auth">
-					<a href="/" className="login">
-						Login
-					</a>
-					<a href="/" className="register">
-						Register
-					</a>
-				</div>
-			</div>
-		</header>
-	);
+        <div className="header__auth">
+          <Link to="/" className="login">
+            Login
+          </Link>
+          <Link to="/" className="register">
+            Register
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
