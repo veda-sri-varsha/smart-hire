a99014bcda986308cafeb9a699db95f05f905ffd
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-28 20:37:03 +0530

    refactor(router): finalize and optimize route setup

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,26 +1,26 @@
 import {
-  createRootRoute,
-  createRoute,
-  createRouter,
+	createRootRoute,
+	createRoute,
+	createRouter,
 } from "@tanstack/react-router";
 import Home from "@/pages/Home";
 import Jobs from "@/pages/Jobs/Jobs";
 import MainLayout from "../../layouts/MainLayout";
 
 const rootRoute = createRootRoute({
-  component: MainLayout,
+	component: MainLayout,
 });
 
 const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/",
-  component: Home,
+	getParentRoute: () => rootRoute,
+	path: "/",
+	component: Home,
 });
 
 const jobsRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: "/jobs",
-  component: Jobs,
+	getParentRoute: () => rootRoute,
+	path: "/jobs",
+	component: Jobs,
 });
 
 const routeTree = rootRoute.addChildren([indexRoute, jobsRoute]);
@@ -28,7 +28,7 @@ const routeTree = rootRoute.addChildren([indexRoute, jobsRoute]);
 export const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
