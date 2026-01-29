4963f6abe8d2487fdad24340b24c29c8e6569a1f
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2026-01-29 16:43:28 +0530

    feat(header): add route-based dark header variant

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,39 @@
 import { JobIcon } from "../Icons";
 import "./Header.scss";
-import { Link } from "@tanstack/react-router";
+import { Link, useRouterState } from "@tanstack/react-router";
 
 const Header = () => {
-  return (
-    <header className="header">
-      <div className="header__container">
-        <div className="header__logo">
-          <span className="header__logo-icon">
-            <JobIcon />
-          </span>
-          <span className="header__logo-text">Job Portal</span>
-        </div>
+	const { location } = useRouterState();
 
-        <nav className="header__nav">
-          <Link to="/">Home</Link>
-          <Link to="/jobs">Jobs</Link>
-          <Link to="/">About Us</Link>
-          <Link to="/">Contact Us</Link>
-        </nav>
+	const isHome = location.pathname === "/";
+	return (
+		<header className={`header ${!isHome ? "header--dark" : ""}`}>
+			<div className="header__container">
+				<div className="header__logo">
+					<span className="header__logo-icon">
+						<JobIcon />
+					</span>
+					<span className="header__logo-text">Job Portal</span>
+				</div>
 
-        <div className="header__auth">
-          <Link to="/" className="login">
-            Login
-          </Link>
-          <Link to="/" className="register">
-            Register
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+				<nav className="header__nav">
+					<Link to="/">Home</Link>
+					<Link to="/jobs">Jobs</Link>
+					<Link to="/about-us">About Us</Link>
+					<Link to="/contact-us">Contact Us</Link>
+				</nav>
+
+				<div className="header__auth">
+					<Link to="/" className="login">
+						Login
+					</Link>
+					<Link to="/" className="register">
+						Register
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
